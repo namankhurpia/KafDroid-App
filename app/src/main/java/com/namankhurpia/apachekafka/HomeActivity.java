d5d5543503b5135d7359598df92bcb51ebbed776
namankhurpia <naman.khurpia2016@vitalum.ac.in>
2022-04-05 15:00:29 +0530

    final commit

--- app/src/main/java/com/namankhurpia/apachekafka/HomeActivity.java
+++ app/src/main/java/com/namankhurpia/apachekafka/HomeActivity.java
@@ -2,10 +2,15 @@ package com.namankhurpia.apachekafka;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.view.WindowManager;
+import android.widget.ImageButton;
 
 public class HomeActivity extends AppCompatActivity {
+
+    ImageButton addcluster;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,5 +18,14 @@ public class HomeActivity extends AppCompatActivity {
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        addcluster = (ImageButton) findViewById(R.id.addcluster);
+        addcluster.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(HomeActivity.this, AddCluster.class));
+            }
+        });
+
     }
 }
